cabef4ac4c4a1300494e851b85c88656e94e237e
MayankSrivastav <mayanksri18@yahoo.in>
2017-02-28 21:18:42 +0530

    Implemented metric conversion

--- js/index.js
+++ js/index.js
@@ -4,22 +4,25 @@ function WeatherViewModel() {
   var self = this;
   self.city = ko.observable('');
   self.temp = ko.observable('');
-  self.sym = ko.observable('℃');
+  self.sym = '℃';
   $.get("http://ipinfo.io", function (response) {
     self.city(response.city);
 
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + self.city() + '&units=metric&APPID=159a04ea6822414e06e035ea47bd0c3b', function (response) {
-      self.temp(response.main.temp + self.sym());
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + self.city() 
+          + '&units=metric&APPID=159a04ea6822414e06e035ea47bd0c3b', function (response) {
+      self.temp(response.main.temp + ' ' + self.sym);
     }, "jsonp");
   }, "jsonp");
 
   self.convert = function () {
-    if (self.sym() === '℃') {
-      var cToFahr = self.temp() * 9 / 5 + 32;
-      self.temp(cToFahr);
+    if (self.sym === '℃') {
+      var cToFahr = (parseFloat(self.temp()) * 9 / 5 + 32).toFixed(2);
+      self.sym = '℉';
+      self.temp(cToFahr + ' ' + self.sym);      
     } else {
-      var fToCel = (self.temp() - 32) * 5 / 9;
-      self.temp(fToCel);
+      var fToCel = ((parseFloat(self.temp()) - 32) * 5 / 9).toFixed(2);
+      self.sym = '℃';
+      self.temp(fToCel + ' ' + self.sym);      
     }
   };
 }
